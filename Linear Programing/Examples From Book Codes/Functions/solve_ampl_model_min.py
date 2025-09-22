f522269bcb5c3682dc749f91b9aef43206fcb9a6
Kevinlevinn7 <kevinlevinn7@hotmail.com>
2025-09-22 14:23:56 -0600

    4444
    444

--- Linear Programing/Examples From Book Codes/Functions/solve_ampl_model_min.py
+++ Linear Programing/Examples From Book Codes/Functions/solve_ampl_model_min.py
@@ -2,30 +2,23 @@ from amplpy import AMPL
 import pandas as pd
 import os
 
-def solve_ampl_model_min(model_file, data_file, solver='gurobi', minimize='Profit', base_path=None):
+def solve_ampl_model_min(model_file, data_file, solver='gurobi', minimize='Profit', base_path=None, show_plan=True):
     ampl = AMPL()  
     if base_path is not None:
         model_file = os.path.join(base_path, model_file)
         data_file = os.path.join(base_path, data_file)
-    
-    # Load model and data
+
     ampl.eval(f"model '{model_file}';")
     ampl.eval(f"data '{data_file}';")
-    
-    # Set solver
     ampl.eval(f"option solver {solver};")
-    
-    # Solve model
     ampl.solve()
-    
-    # Print objective value
     total_obj = ampl.get_objective(f"Total_{minimize}")
-    print(f"Total {minimize}: {total_obj.value()}")
-    
-    # Print variable values
     make = ampl.get_variable('Make')
     make_df = make.get_values().to_pandas()
 
-    print("\nProduction plan:")
-    print(make_df)
+    if show_plan: #Display plans etc.
+        print(f"Total {minimize}: {total_obj.value()}")
+        print("\nProduction plan:")
+        print(make_df)
+    
     return ampl
--- Linear Programing/Examples From Book Codes/Problem 2-6 AMPL Book/paper_mill2_int.mod
+++ Linear Programing/Examples From Book Codes/Problem 2-6 AMPL Book/paper_mill2_int.mod
@@ -5,8 +5,7 @@ param order {W} > 0;  # order for each width
 param a {W,P} >= 0;  # number of rolls of width w in pattern p  
 param width_roll >= 0;
 
-
-var Make {P} >= 0; 
+var Make {P} integer >= 0; 
 
 minimize Total_Number: sum {p in P} Make[p];
 
